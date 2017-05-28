73c2c6bcba3b0e5b16d4215199c2ac84fe9fa322
Mélissa Rissel <rissel.melissa@gmail.com>
2017-05-28 23:59:25 +0100

    maybethirds

--- sections/two_thirds_one_third.php
+++ sections/two_thirds_one_third.php
@@ -1,19 +1,21 @@
-<?php $small_position = get_sub_field('big_position'); ?> 
-<?php  $classes = ($small_position == 'right') ?  [ 'col-sm-4 col-sm-push-8' ,'col-sm-8 col-sm-pull-4', ]  :  ['col-sm-8', 'col-sm-4']  ; ?> 
+<?php $small_position = get_sub_field('small_position'); ?> 
+<?php $bg_small = get_sub_field('bg_small');?>
+<?php $bg_big = get_sub_field('bg_big');?>
+<?php  $classes = ($small_position == 'right') ?  [ 'col-sm-8' ,'col-sm-4', ]  :  ['col-sm-8 col-sm-push-4', 'col-sm-4 col-sm-pull-8']  ; ?> 
 
-
+<div style="margin: 0 15px;">
 	<div class="row">
 
 
-		<div class="<?php echo $classes[0]; ?>">
+		<div class="<?php echo $classes[0] . ' ' . $bg_big; ?> bigcol">
 		<?php echo get_sub_field('two_thirds_content'); ?>
 		</div>
 
 
-		<div class="<?php echo $classes[1]; ?>">
+		<div class="<?php echo $classes[1] . ' ' . $bg_small; ?> smallcol">
 			<?php echo get_sub_field('one_third_content'); ?>
 		</div>
 
 	</div> <!-- END OF ROW -->
 
-
+</div>
