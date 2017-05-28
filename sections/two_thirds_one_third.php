19f223d24cc97a91ed8bf0414f761bb8676a83be
Mélissa Rissel <rissel.melissa@gmail.com>
2017-05-28 22:54:22 +0100

    2/3-1/3 start

--- sections/two_thirds_one_third.php
+++ sections/two_thirds_one_third.php
@@ -1,8 +1,7 @@
-<?php $image =  get_sub_field('one_third_image'); ?>
-<?php $image_position = get_sub_field('image_position'); ?> 
-<?php  $classes = ($image_position == 'right') ?  [ 'col-sm-8 col-sm-text-right', 'col-sm-4' ]  :  [ 'col-sm-8 col-sm-push-4', 'col-sm-4 col-sm-pull-8' ]  ; ?> 
+<?php $small_position = get_sub_field('big_position'); ?> 
+<?php  $classes = ($small_position == 'right') ?  [ 'col-sm-4 col-sm-push-8' ,'col-sm-8 col-sm-pull-4', ]  :  ['col-sm-8', 'col-sm-4']  ; ?> 
 
-<div class="container">
+
 	<div class="row">
 
 
@@ -12,9 +11,9 @@
 
 
 		<div class="<?php echo $classes[1]; ?>">
-		<div class="one_third_image"><img src="<?php echo $image['url']; ?>"  alt="" /></div>
+			<?php echo get_sub_field('one_third_content'); ?>
 		</div>
 
 	</div> <!-- END OF ROW -->
 
-</div><!--  END OF CONTAINER -->
+
