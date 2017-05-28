4b2abee2865291592398317f840282b151b16c54
Mélissa Rissel <rissel.melissa@gmail.com>
2017-05-28 22:32:02 +0100

    title section

--- page.php
+++ page.php
@@ -6,7 +6,7 @@
 <!-- section -->
 <section >
 
-	<h1 class="container"><?php the_title(); ?></h1>
+	<!-- <h1 class="container"><?php the_title(); ?></h1> -->
 
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
